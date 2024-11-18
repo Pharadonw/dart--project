4acf4aac662fdb63818de68555e6a727b68ebad0
Pharadon <pharadonw65@nu.ac.th>
2024-11-18 16:53:35 +0700

    เเก้เเล้วครับ

--- reminder.dart
+++ reminder.dart
@@ -1,3 +1,12 @@
-void main(){
-  print("Hallo sir");
+void main() {
+  int num1 = 10;  // ตัวตั้ง
+  int num2 = 3; // ตัวหาร
+ 
+  // คำนวณผลหารและเศษ
+  int quotient = num1 ~/ num2;  // ผลหารที่เป็นจำนวนเต็ม
+  int remainder = num1 % num2; // ค่าเศษ
+ 
+  // แสดงผล
+  print('quotient = $quotient');
+  print('remainder = $remainder');
 }
